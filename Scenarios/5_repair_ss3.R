1f594bbe68ee35e14c2be3e9f69a1e164533dd69
Adyan Rios <adyan.b.rios@gmail.com>
2025-04-02 09:04:25 -0400

    new script to make a change to many runs

---
+++ Scenarios/5_repair_ss3.R
@@ -0,0 +1,43 @@
+library(foreach)
+
+# Specify pattern
+pattern <- "84_stx_f3_5cm_010641_0041_"
+
+# Get folder names ####
+full_names <- list.files(
+  path = here::here("Scenarios"),
+  pattern = pattern,
+  full.names = TRUE
+)
+
+foreach::foreach(i = seq_along(full_names)) %do% {
+
+  start <- r4ss:::SS_readstarter(here::here(full_names[i], "starter.ss"),
+                                 verbose = FALSE)
+  dat <- r4ss:::SS_readdat(file = here::here(full_names[i], start$datfile),
+                           version = 3.3, verbose = FALSE)
+  ctl <- r4ss::SS_readctl(file = here::here(full_names[i], start$ctlfile),
+                          datlist = dat, verbose = FALSE)
+  
+  
+  # Update M
+  ctl$MG_parms
+  ctl$MG_parms[1,3]
+  ctl$MG_parms[1,3] = 0.27
+  
+  # r4ss::SS_writedat(
+  #   datlist = dat,
+  #   outfile = here::here(full_names[i], start$datfile),
+  #   overwrite = TRUE,
+  #   verbose = FALSE
+  # )
+  # 
+  
+  r4ss::SS_writectl(
+    ctllist = ctl,
+    outfile = here::here(full_names[i], start$ctlfile),
+    overwrite = TRUE
+  )
+  
+  return(NULL)
+}
